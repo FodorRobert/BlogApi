e4eb199401af64cc10e5c5370bcf61d0a1045840
fodorr <fodorr@kkszki.hu>
2026-09-09 10:12:55 +0200

    MySqlConnector package is added

--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525115e7e73cf2d9b4868ec1978e4183c3cbef87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4a6ba30cba15811d08b2aca71373c4af3f2666")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
